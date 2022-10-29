11a2648b42f1eb98a2cff21ead3de59ea60fea78
Rishabh Tripathi <rishabhtripathi0319@gmail.com>
2022-10-29 19:27:05 +0530

    Assignment 1 is Done on 29/10/22 and submitted on 29/10/22

---
+++ Assignment 1/program.c
@@ -0,0 +1,165 @@
+
+/* ########### Assignment-1 ############ */
+/* ########### input and output in C language ############ */
+
+
+/* *********************** 1. Write a program to print Hello Students on the screen. ************************* */
+
+// #include<stdio.h>
+// void main()
+// {
+//    printf("Hello Students");
+// }
+
+
+
+
+/* *********************** 2. Write a program to print Hello in the first line and Students in the second line. ************************* */
+
+// #include<stdio.h>
+// void main()
+// {
+//    printf("Hello\nStudents");
+// }
+
+
+
+
+/* *********************** 3. Write a program to print “iNeuron” on the screen. (Remember to print in double quotes) ************************* */
+
+// #include<stdio.h>
+// void main()
+// {
+//    printf("\"iNeuron\"");
+// }
+
+
+
+
+/* *********************** 4. WAP to find the area of the circle. Take radius of circle from user as input and print the result in below given format. ************************* */
+
+//Expected output format – “Area of circle is A having the radius R”. Replace A with area & R with radius.
+
+
+// #include<stdio.h>
+// float findAreaOfCircle(int);   // function decliration
+
+// void main()
+// {
+   
+//    int radius; 
+//    printf("Enter Radius of Circle: ");
+//    scanf("%d",&radius);
+//    float area = findAreaOfCircle(radius);
+
+//    printf("area of circle is %.2f having the circle %d",area,radius);
+// }
+
+// float findAreaOfCircle(int radius)
+// {
+//    float pie = 3.142; 
+//    return (pie * (radius * radius));   
+// }
+
+
+
+
+/* *********************** 6. WAP to print the name of the user in double quotes. *********************** */
+
+
+// #include<stdio.h>
+// void main()
+// {
+//    printf("\"Hello , Rishabh\"");
+// }
+
+
+
+
+/* *********************** 7. WAP to print “%d” on the screen. *********************** */
+
+
+// #include<stdio.h>
+// void main()
+// {
+//     printf("%%d");
+// }
+
+
+
+/* *********************** 8. WAP to print “\n” on the screen. *********************** */
+
+// #include <stdio.h>
+// void main()
+// {
+//    printf("\\n");
+// }
+
+
+/* *********************** 9. WAP to print “\\” on the screen. *********************** */
+
+
+// #include<stdio.h>
+// void main()
+// {
+//    printf("\\\\");
+// }
+
+
+
+/* *********************** 10. WAP to take date as an input in below given format and convert the date format and display the result as given below. *********************** */
+
+// User Input date format – “DD/MM/YYYY” (27/11/2022)
+
+// Output format – “Day – DD , Month – MM , Year – YYYY” (Day – 27 ,Month – 07 , Year – 2022)
+
+
+// #include<stdio.h>
+// void main()
+// {
+//    int day,month,year;
+
+//    printf("\n\nEnter today Date in (DD/MM/YYYY) formate: ");
+//    scanf("%d/%d/%d",&day,&month,&year);
+
+//    printf("\n\nDay - %d , Month - %d , Year - %d\n\n",day,month,year);
+
+// }
+
+
+
+/* *********************** 11. WAP to take time as an input in below given format and convert the time format and display the result as given below. *********************** */
+
+
+
+// #include<stdio.h>
+// void main()
+// {
+//    int hour,minute; 
+//    printf("\nEnter current time in (HH:MM) Format: ");
+//    scanf("%d:%d",&hour,&minute);
+
+//    printf("\n%d Hour and %d Minute\n\n",hour,minute);
+
+// }
+
+
+
+
+
+
+/* *********************** 12. Find output of below code: *********************** */
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int x = printf("ineuron");
+//     printf("%d",x);
+
+//     return 0;
+// }
+
+// OUTPUT IS ==> ineuron7
+
